9b7cc9f837da7f495e37841fb965ad6c103ac35d
Ayris Gürbulak <ayriisgr@gmail.com>
2022-02-23 17:07:17 +0300

    update folder

--- restaurants-project/AppDelegate.swift
+++ restaurants-project/AppDelegate.swift
@@ -3,7 +3,7 @@ import Firebase
 import GoogleMaps
 import CoreData
 
-let googleAPIKey = "AIzaSyDHIw4Tv0b1m3cbP1JZGMewPIFpR9A9SBU"
+let googleAPIKey = "ENTER_KEY"
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
